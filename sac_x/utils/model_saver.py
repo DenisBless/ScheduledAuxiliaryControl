3b8633c37f2895041751482b5f86470df6965e11
blessing <blessing.denis@gmx.de>
2020-08-05 19:06:58 +0200

    added a model saver

---
+++ sac_x/utils/model_saver.py
@@ -0,0 +1,25 @@
+import os
+import datetime
+import torch
+import pathlib
+
+
+class ModelSaver:
+    def __init__(self, model_root_dir=None, save_every=10):
+        if model_root_dir is None:
+            model_root_dir = str(pathlib.Path(__file__).resolve().parents[2]) + "/models/"
+
+        if not os.path.isdir(model_root_dir):
+            os.mkdir(model_root_dir)
+        self.model_dir = model_root_dir + datetime.datetime.now().strftime("%d-%m_%H-%M")
+
+        if not os.path.isdir(self.model_dir):
+            os.mkdir(self.model_dir)
+
+        self.save_every = save_every
+        self.ctr = 0
+
+    def save_model(self, model, name: str):
+        if self.ctr != 0 and self.ctr % self.save_every == 0:
+            torch.save(model, self.model_dir + "/" + name + "_" + self.ctr)
+        self.ctr +=1
