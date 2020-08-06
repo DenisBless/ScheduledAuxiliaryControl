e849576001e489eafbc541ba95f8ec8c4b7924e0
blessing <blessing.denis@gmx.de>
2020-08-06 10:45:36 +0200

    added sacQ scheduler

--- experiments/run.sh
+++ experiments/run.sh
@@ -1,11 +1,12 @@
-python3 ../sac_x/main.py \
+python3 -O ../sac_x/main.py \
 --num_worker=6 \
 --num_grads=6 \
 --update_targnets_every=300 \
 --learning_steps=1200 \
 --actor_lr=2e-4 \
 --critic_lr=2e-4 \
---global_gradient_norm=0.5 \
+--global_gradient_norm=5 \
 --entropy_reg=1e-2 \
 --replay_buffer_size=2000 \
 --num_trajectories=20  \
+--num_intentions=14
